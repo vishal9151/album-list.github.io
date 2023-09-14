02d151790de3bc5cb572e33a435839e39338b509
vishal9151 <84173784+vishal9151@users.noreply.github.com>
2023-09-14 17:13:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# album-list.github.io
+# Albums
+Building a Dynamic Albums Management App using the power of React for front-end development, Tailwind CSS for styling, and Axios for handling HTTP requests.
+
+# Features
+- Add new albums with a custom title.
+- Delete existing albums from the list.
+- Update existing title with a custom title.
+- Efficient use of React and Axios for data management.
+- Utilizes Tailwind CSS for styling and layout.
+
+# Folder Structure
+The project follows the following structure:
+- `/public`: Directory containing public assets.
+  - `index.html`: Main HTML file.
+  - `favicon.ico`: Favicon Icon.
+- `/src`: Source code directory.
+  - `/components`: Directory for React components.
+    - `AddAlbumButton.js`: Component for adding new albums.
+    - `UpdateAlbumButton.js`: Component for updating album titles.
+  - `App.js`: Main application component.
+  - `index.js`: Entry point of the application.
+  - `index.css`: Styles.
+- `tailwind.config.css`: Tailwind CSS configuration.
+- `package.json`: Project's package configuration.
+- `README.md`: The document you are reading right now.
+
+# Getting Started
+Follow these steps to set up and run the project on your local machine.
+
+## Prerequisites
+- Node.js (npm will be installed with Node.js)
+  
+## Installation
+1. Clone the repository:
+```bash
+git clone 
+```
+
+2. Navigate to the project directory:
+```bash
+cd albums-management-app
+```
+
+3. Install dependencies:
+```bash
+npm install
+```
+
+4. Start the development server:
+```bash
+npm run start
+```
+
+5. Access the app in your browser at http://localhost:3000.
+   
+# Usage
+- Click the "Add Album" button to create new albums.
+- Use the "Update" button to modify album titles.
+- Press the "Delete" button to remove albums from the list.
+
+# Live Demo
+Check out the live demo of the App: 
---
+++ src/components/UpdateAlbumButton.js
@@ -0,0 +1,97 @@
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+
+const UpdateAlbumButton = ({ albumId, albums, setAlbums }) => {
+
+    const [selectedUpdatedAlbum, setSelectedUpdatedAlbum] = useState(false); // State for controlling the update panel
+    const [updatedAlbumTitle, setUpdatedAlbumTitle] = useState(''); // State for storing the updated album title
+
+    // Function to handle update album and make a PUT request to an API.
+    const updateAlbum = () => {
+        if (updatedAlbumTitle !== "") {
+            axios.put(`https://jsonplaceholder.typicode.com/albums/${albumId}`,
+                { title: updatedAlbumTitle, }
+            )
+                .then(response => {
+                    // Update the albums array with the new title
+                    const updatedAlbums = albums.map(album =>
+                        album.id === albumId ? { ...album, title: updatedAlbumTitle } : album
+                    );
+                    setAlbums(updatedAlbums);
+                    setSelectedUpdatedAlbum(false);
+                    setUpdatedAlbumTitle('');
+                })
+                .catch(error => {
+                    alert('Error updating album : ' + error.message);
+                });
+        }
+        else{
+            alert("Please write something in input box to update an item.");
+      }
+    };
+
+    // Ref for the updateAlbumButton panel
+    const updateAlbumRef = useRef(null);
+
+    // Effect to close the update panel that appears when selectedUpdatedAlbum is true when clicking outside of the box
+    useEffect(() => {
+        document.body.addEventListener("click", (e) => {
+            if (e.target.contains(updateAlbumRef.current)) {
+                setSelectedUpdatedAlbum(false);
+                setUpdatedAlbumTitle('');
+            };
+        })
+    }, [updateAlbumRef])
+
+    return (
+        <div>
+            {/* Button to toggle the update panel */}
+            <button
+                onClick={() => setSelectedUpdatedAlbum(!selectedUpdatedAlbum)}
+                className="bg-blue-500 hover:bg-white font-semibold text-white hover:text-blue-500 px-2 py-1 rounded border hover:border-green-500"
+            >Update</button>
+
+            {selectedUpdatedAlbum &&
+                // The update panel that appears when selectedUpdatedAlbum is true
+                <div className='w-screen h-screen fixed z-50 top-0 left-0 flex items-center justify-center'
+                    style={{ background: 'grey' }}>
+                    <div ref={updateAlbumRef} className=" w-[50%] rounded-lg  "
+                        style={{ background: 'lightgrey' }}>
+
+                        <div className='p-3 flex flex-col gap-8'>
+                            <div>
+                                <p className='text-xl font-semibold mb-3'>Update Album</p>
+
+                                {/* Input for the updated title */}
+                                <input
+                                    type="text"
+                                    placeholder="Updated title"
+                                    value={updatedAlbumTitle}
+                                    onChange={event => setUpdatedAlbumTitle(event.target.value)}
+                                    className="border rounded px-2 py-1 w-full"
+                                />
+                            </div>
+
+                            <div className='w-full flex justify-between   items-center'>
+
+                                {/* Update button */}
+                                <button
+                                    onClick={updateAlbum}
+                                    className="bg-green-500 hover:bg-white font-semibold text-white hover:text-green-500 px-2 py-1 rounded border hover:border-green-500"
+                                >Update</button>
+
+                                {/* Cancel button */}
+                                <button
+                                    onClick={() => { setSelectedUpdatedAlbum(!selectedUpdatedAlbum); setUpdatedAlbumTitle("") }}
+                                    className="bg-red-600 hover:bg-white font-semibold text-white hover:text-red-600 px-2 py-1 rounded border hover:border-red-600"
+                                >Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    );
+}
+
+export default UpdateAlbumButton;
